af3ee06f9091bfcfe42d30b29d4ff1b8d9da4ece
Prashant <prashan1837d@gmail.com>
2026-02-08 21:34:19 +0530

    Redesign calendar component, remove focus ring, and fix typos

--- app/(routes)/details/_components/BookingSection.jsx
+++ app/(routes)/details/_components/BookingSection.jsx
@@ -101,40 +101,42 @@ function BookingSection({ children, business }) {
         <SheetTrigger asChild>{children}</SheetTrigger>
         <SheetContent className="overflow-auto">
           <SheetHeader>
-            <SheetTitle>Book an Service</SheetTitle>
+            <SheetTitle>Book a Service</SheetTitle>
             <SheetDescription>
-              Select Date and Time slot to book an service
-              {/* Date Picker  */}
-              <div className="flex flex-col gap-5 items-baseline">
-                <h2 className="mt-5 font-bold">Select Date</h2>
+              Select Date and Time slot to book a service
+            </SheetDescription>
+          </SheetHeader>
 
-                <Calendar
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  className="rounded-md border "
-                />
-              </div>
-              {/* Time Slot Picker  */}
-              <h2 className="my-5 font-bold">Select Time Slot</h2>
-              <div className="grid grid-cols-3 gap-3">
-                {timeSlot.map((item, index) => (
-                  <Button
-                    key={index}
-                    disabled={isSlotBooked(item.time)}
-                    variant="outiline"
-                    className={`border rounded-full 
+          {/* Date Picker  */}
+          <div className="flex flex-col gap-5 items-start">
+            <h2 className="mt-5 font-bold">Select Date</h2>
+
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              className="rounded-md border "
+              fromDate={new Date()}
+            />
+          </div>
+          {/* Time Slot Picker  */}
+          <h2 className="my-5 font-bold">Select Time Slot</h2>
+          <div className="grid grid-cols-3 gap-3">
+            {timeSlot.map((item, index) => (
+              <Button
+                key={index}
+                disabled={isSlotBooked(item.time) || moment(date).isBefore(moment(), 'day') || (moment(date).isSame(moment(), 'day') && moment().isAfter(moment(item.time, "h:mm A")))}
+                variant="outline"
+                className={`border rounded-full 
                 p-2 px-3 hover:bg-primary
                  hover:text-white
                  ${selectedTime == item.time && "bg-primary text-white"}`}
-                    onClick={() => setSelectedTime(item.time)}
-                  >
-                    {item.time}
-                  </Button>
-                ))}
-              </div>
-            </SheetDescription>
-          </SheetHeader>
+                onClick={() => setSelectedTime(item.time)}
+              >
+                {item.time}
+              </Button>
+            ))}
+          </div>
           <SheetFooter className="mt-5">
             <SheetClose asChild>
               <div className="flex gap-5">
